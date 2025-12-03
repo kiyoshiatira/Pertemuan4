3c8c3c8d32e3ccb27aa3ac61a44aff9d3e3dfed6
Kiyoshi <kiyoshi.atira@gmail.com>
2025-12-03 08:47:21 +0700

    first commit

---
+++ uji3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+int main (argc, argv )
+	int argc ;
+	char *argv[ ] ;
+{
+	execv ("/bin/echo", /* program yang dimuat – hanya full path */
+			&argv[0] ) ;
+	printf ("EXEC Failed\n") ;
+	/* Baris di atas akan dicetak saat terdapat kesalahan */
+}
